4cbe2a8efd99b8feb28d51fc298386a6ed4ccdb7
Mayank Jha <78186044+mayankjha-purdue@users.noreply.github.com>
2021-06-12 15:46:55 -0400

    Add files via upload

--- create_secret.sh
+++ create_secret.sh
@@ -9,15 +9,18 @@ GCS_ENCODED=$(cat $GOOGLE_APPLICATION_CREDENTIALS | base64 -w 0)
 
 # Substitute those creds into our secrets file
 sed -i -e 's|'REPLACE_GCS_CREDS'|'"$GCS_ENCODED"'|g' secret.json
+sed -i -e 's|'REPLACE_STORAGE_BUCKET'|'"$STORAGE_BUCKET"'|g' secret.json
 
 # Encode our SSL certs
-SSLROOTCERT_ENCODED=$(echo $PG_SSLROOTCERT | base64 -w 0)
-SSLCERT_ENCODED=$(echo $PG_SSLCERT | base64 -w 0)
-SSLKEY_ENCODED=$(echo $PG_SSLKEY | base64 -w 0)
+SSLROOTCERT_ENCODED=$(cat $PG_SSLROOTCERT | base64 -w 0)
+SSLCERT_ENCODED=$(cat $PG_SSLCERT | base64 -w 0)
+SSLKEY_ENCODED=$(cat $PG_SSLKEY | base64 -w 0)
 
 # Substitute those creds into our secrets file
 sed -i -e 's|'REPLACE_PG_HOST'|'"$PG_HOST"'|g' secret_db.json
 sed -i -e 's|'REPLACE_PG_PASSWORD'|'"$PG_PASSWORD"'|g' secret_db.json
+sed -i -e 's|'REPLACE_PG_DBNAME'|'"$PG_DBNAME"'|g' secret_db.json
+sed -i -e 's|'REPLACE_PG_USER'|'"$PG_USER"'|g' secret_db.json
 sed -i -e 's|'REPLACE_PG_SSLROOTCERT'|'"$SSLROOTCERT_ENCODED"'|g' secret_db.json
 sed -i -e 's|'REPLACE_PG_SSLCERT'|'"$SSLCERT_ENCODED"'|g' secret_db.json
 sed -i -e 's|'REPLACE_PG_SSLKEY'|'"$SSLKEY_ENCODED"'|g' secret_db.json
